4085b457518deaf78c5187a0439cbff2a8499795
Rishisaiganesh <crazyganesh67@gmail.com>
2021-07-02 20:01:15 +0530

    Inserting to Table

--- ABook.sql
+++ ABook.sql
@@ -10,4 +10,10 @@ zipcode int,
 PhoneNumber varchar(255),
 email varchar(255),
 );
-
+insert into Addressdetails
+values ('Ravi','kumar','RRcomplex','Nellore','AndhraPradesh',524001,'6546546541','254asd@gmail.com'),
+('Mohan','Reddy','Balaji Street','Vizag','Andhrapradesh','500036','7644566278','mohan@gmail.com'),
+('Madhu','Roy','abc Street','Vizayawada','Andhrapradesh','500056','7644565778','madhu@gmail.com'),
+('yeshwanth','Jona','def Street','Banglore','Karnataka','500066','7645676278','john@gmail.com'),
+('Martin','Setty','hij Street','Indore','Punjab','505536','7644564568','martin@gmail.com')
+select*from Addressdetails
