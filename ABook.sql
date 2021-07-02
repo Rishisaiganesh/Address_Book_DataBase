32a913fafb32a8e300e465ae5d91742a8fc52252
Rishisaiganesh <crazyganesh67@gmail.com>
2021-07-02 19:54:31 +0530

    CreatingTable

--- ABook.sql
+++ ABook.sql
@@ -1,1 +1,13 @@
 create database AddressBookService;
+use AddressBookService;
+create table Addressdetails(
+firstName varchar(255),
+lastName varchar(255),
+addresss varchar(255),
+city varchar(255),
+statee varchar(255),
+zipcode int,
+PhoneNumber varchar(255),
+email varchar(255),
+);
+
